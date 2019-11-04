f686f24121220f44d2d4e4fe0c0ec2f4345e3f42
WpavanN <pavan.wadhe@gmail.com>
2019-11-04 22:24:36 +1100

    removed flight details. Back to 2 tables

--- Qantas/flightinfoapi.py
+++ Qantas/flightinfoapi.py
@@ -3,63 +3,58 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 import sqlite3 as lite
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_, or_, not_
+from sqlalchemy import create_engine
+
+
 
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'flightinfodata.db')
+engine = create_engine('sqlite:///' + os.path.join(basedir, 'flightinfodata.db'))
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
-class flightDetails(db.Model):
-    flightNumber = db.Column(db.String(80), primary_key=True)
-    airline = db.Column(db.String(80), nullable=False)
-    
-
-    def __init__(self, flightNumber, airline):
-        self.flightNumber = flightNumber
-        self.airline = airline
-        
-
-class fltDetailSchema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ('flightNumber', 'airline')
+Session = sessionmaker(bind = engine)
+session = Session()
 
 class departure(db.Model):
     flightNumber = db.Column(db.String(80), primary_key=True)
     scheduled = db.Column(db.String(100), nullable = False)
     airport = db.Column(db.String(150), nullable = False)
+    airline = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, flightNumber, airline, scheduled, airport):
+
+    def __init__(self, flightNumber,airline, scheduled, airport):
         self.flightNumber = flightNumber
-        self.airline = airline
         self.scheduled = scheduled
         self.airport = airport
+        self.airline = airline
 
 class DeptSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('flightNumber', 'scheduled', 'airport')
+        fields = ('flightNumber', 'scheduled', 'airport','airline')
 
 class arrival(db.Model):
     flightNumber = db.Column(db.String(80), primary_key=True)
     scheduled = db.Column(db.String(100), nullable = False)
     airport = db.Column(db.String(150), nullable = False)
+    airline = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, flightNumber, airline, scheduled, airport):
+
+    def __init__(self, flightNumber, scheduled, airport):
         self.flightNumber = flightNumber
-        self.airline = airline
         self.scheduled = scheduled
         self.airport = airport
+        self.airline = airline
 
 class ArrSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('flightNumber', 'scheduled', 'airport')
+        fields = ('flightNumber', 'scheduled', 'airport', 'airline')
 
-flight_detail_schema = fltDetailSchema()
-flight_details_schema = fltDetailSchema(many=True)
+
 dept_schema = DeptSchema()
 depts_schema = DeptSchema(many=True)
 arr_schema = ArrSchema()
@@ -68,8 +63,12 @@ arrs_schema = ArrSchema(many=True)
 # endpoint to show all users
 @app.route("/flights", methods=["GET"])
 def get_user():
-    flight_details = arrival.query.all()
-    result = arrs_schema.dump(flight_details)
+    print("Hello, this in the api")
+    # flight_details = session.query(departure,arrival).filter(and_(arrival.flightNumber=departure.flightNumber,arrival.airline=="Qantas",(or_(arrival.airport=="SYD",departure.airport=="SYD")))).all()
+    for d,a in session.query(departure,arrival).filter(and_(arrival.flightNumber=departure.flightNumber,arrival.airline=="Qantas",(or_(arrival.airport=="SYD",departure.airport=="SYD")))).all():
+        print("this is the airline:{}".format(d.airline))
+    
+    # result = arrs_schema.dump(flight_details)
     return jsonify(result.data)
 
 if __name__ == '__main__':
